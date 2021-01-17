84b6dbefbd3a9d92629e433b5423b2cef96792b8
Scoowy <gahonajuanjo@gmail.com>
2021-01-16 23:16:02 -0500

    Added CORS

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 
 import ApiRoutes from "./routes/api.routes";
 
@@ -8,6 +9,8 @@ const app = express();
 // Settings
 app.set("port", process.env.PORT || 3000);
 
+const corsOptions = {};
+app.use(cors(corsOptions));
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
