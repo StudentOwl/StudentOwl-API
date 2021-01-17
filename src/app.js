941161ce97beaa224dc92c9e25ce5a5f70102d81
Scoowy <gahonajuanjo@gmail.com>
2021-01-16 23:03:31 -0500

    Content-Type: application/json

--- src/app.js
+++ src/app.js
@@ -3,8 +3,12 @@ import ApiRoutes from "./routes/api.routes";
 
 const app = express();
 
+// Settings
 app.set("port", process.env.PORT || 3000);
 
+app.use(express.json());
+
+// Routes
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to StudentOwl-API" });
 });
