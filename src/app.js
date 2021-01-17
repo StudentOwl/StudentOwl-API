da3fa202062ca4acc8864ed8f2d19f3af6083ad3
Scoowy <gahonajuanjo@gmail.com>
2021-01-16 23:12:48 -0500

    Added compatibility with html forms

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ app.set("port", process.env.PORT || 3000);
 
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // Routes
 app.get("/", (req, res) => {
