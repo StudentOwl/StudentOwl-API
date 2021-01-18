1cdebbf5c5dd4eb16e27b9ebd658a486a2ccfb42
Scoowy <gahonajuanjo@gmail.com>
2021-01-18 10:57:01 -0500

    Params to query

--- src/controllers/logs.controller.js
+++ src/controllers/logs.controller.js
@@ -64,81 +64,26 @@ export const saveNewLogs = async (req, res, next) => {
   }
 };
 
-// export const findOneComponent = async (req, res, next) => {
-//   const Component = (await logsDb).model("Component");
+export const findAllLogsByComponentQueryTest = async (req, res, next) => {
+  const Log = (await logsDb).model(req.params.component);
 
-//   try {
-//     const component = await Component.findById(req.params.id);
+  const filter = {};
 
-//     if (component) {
-//       res.json({ status: "Correct", data: component });
-//     } else {
-//       next(itemNotFound(req.params.id));
-//     }
-//   } catch (err) {
-//     next(errorUndefined(err));
-//   }
-// };
+  if (req.params.student) {
+    filter["student"] = req.params.student;
+  }
+  if (req.query.msStart) {
+    filter["time"] = { $gte: new Date(parseInt(req.query.msStart)) };
+  }
+  if (req.query.msEnd) {
+    filter["time"]["$lt"] = new Date(parseInt(req.query.msEnd));
+  }
 
-// export const createComponent = async (req, res, next) => {
-//   const Component = (await logsDb).model("Component");
-//   const newComponent = new Component({
-//     _id: req.body._id,
-//     name: req.body.name,
-//     teacher: req.body.teacher,
-//     students: req.body.students ? req.body.students : []
-//   });
-
-//   try {
-//     await newComponent.save();
-//     res.status(201).json({ status: "Created", data: newComponent });
-//   } catch (err) {
-//     next(errorUndefined(err));
-//   }
-// };
-
-// export const updateComponent = async (req, res, next) => {
-//   const Component = (await logsDb).model("Component");
-
-//   try {
-//     const component = await Component.findByIdAndUpdate(
-//       req.params.id,
-//       req.body
-//     );
-//     if (component) {
-//       res.json({ status: "Updated", data: component });
-//     } else {
-//       next(itemNotFound(req.params.id));
-//     }
-//   } catch (err) {
-//     next(errorUndefined(err));
-//   }
-// };
-
-// export const deleteComponent = async (req, res, next) => {
-//   const Component = (await logsDb).model("Component");
-
-//   try {
-//     const deleted = await Component.findByIdAndDelete(req.params.id);
-//     if (deleted) {
-//       res.json({ status: `Deleted ${deleted._id} item` });
-//     } else {
-//       next(itemNotFound(req.params.id));
-//     }
-//   } catch (err) {
-//     next(errorUndefined(err));
-//   }
-// };
-
-// export const deleteAllComponent = async (req, res, next) => {
-//   const Component = (await logsDb).model("Component");
-
-//   try {
-//     const deleteds = await Component.deleteMany({});
-//     if (deleteds) {
-//       res.json({ status: `Deleted ${deleteds.length} items` });
-//     }
-//   } catch (err) {
-//     next(errorUndefined(err));
-//   }
-// };
+  console.log(filter);
+  try {
+    const logs = await Log.find(filter);
+    res.json({ status: "Correct", count: logs.length, data: logs });
+  } catch (err) {
+    next(errorUndefined(err));
+  }
+};
--- src/routes/version10/log.routes.js
+++ src/routes/version10/log.routes.js
@@ -6,6 +6,10 @@ const router = Router();
 router.param("component", LogCtrl.capitalizeComponentId);
 
 router
+  .route("/:component(\\w{4}\\d{2})/")
+  .get(LogCtrl.findAllLogsByComponentQueryTest);
+
+router
   .route("/:component(\\w{4}\\d{2})/:msStart(\\d{13})?-?:msEnd(\\d{13})?")
   .get(LogCtrl.findAllLogsByComponent);
 router
