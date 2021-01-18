35d81eb1faa1ee6dc58031fb26d9d1d7350e91c8
Scoowy <gahonajuanjo@gmail.com>
2021-01-17 19:34:12 -0500

    Modified env variables fallback

--- src/config.js
+++ src/config.js
@@ -2,6 +2,8 @@ import { config } from "dotenv";
 config();
 
 export default {
-  MONGODB_GENERAL: process.env.MONGODB_URI || "mongodb://localhost/database",
-  MONGODB_LOGS: process.env.MONGODB_LOGS || "mongodb://localhost/database"
+  MONGODB_GENERAL:
+    process.env.MONGODB_URI || `mongodb://localhost/${process.env.DB_API}`,
+  MONGODB_LOGS:
+    process.env.MONGODB_LOGS || `mongodb://localhost/${process.env.DB_LOGS}`
 };
